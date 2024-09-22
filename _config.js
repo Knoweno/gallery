dd52c07f15c150d5220a5c23aaa61e22dbed9f20
Knowen.Obiso <knowenemmanuel@gmail.com>
2024-09-22 23:25:44 +0300

    Update db connection

--- _config.js
+++ _config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-const mongoBaseURL = 'mongodb+srv://knowenobiso:DWKMiPk7rPzEOkId@moringadevops.b9yyp.mongodb.net/?retryWrites=true&w=majority&appName=MoringaDevOps';
+const mongoBaseURL = 'mongodb+srv://knowenobiso:DWKMiPk7rPzEOkId@moringadevops.b9yyp.mongodb.net/darkroom?retryWrites=true&w=majority&appName=MoringaDevOps';
 
 //'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
 // Update to have your correct username and password
